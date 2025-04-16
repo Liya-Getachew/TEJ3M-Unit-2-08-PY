d6618a14e5ea6d2685f4c931d06f9540f310963e
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2025-04-16 11:08:21 -0400

    Update code.py

--- code.py
+++ code.py
@@ -1,34 +1,36 @@
-# SPDX-FileCopyrightText: 2018 Kattni Rembor for Adafruit Industries
-#
-# SPDX-License-Identifier: MIT
+"""
+Created by: Liya Getachew
+Created: 08/04/25
 
-# Liya Getachew
-# 08/04/25
-#
-# Move a servo with a potentiometer in sync
+Move a servo with a potentiometer in sync
+"""
 
 import time
 import board
 import pwmio
-from adafruit_motor import servo
+import servo
 from analogio import AnalogIn
 
-# create a PWMOut object on Pin 16.
+
+# sets servo pulse width modulation on Pin 16
 pwm = pwmio.PWMOut(board.GP16, duty_cycle=2 ** 15, frequency=50)
 
-potentionmeter = AnalogIn(board.GP26) # sets pot pin to pin 26/A0
+# sets potentiometer pin to pin 26 (analog pin 0)
+potentionmeter = AnalogIn(board.GP26)
 
-# Create a servo object, my_servo.
+# Creates a servo object called my_servo
 my_servo = servo.Servo(pwm, min_pulse = 650, max_pulse = 2500)
 
 while True:
     print((potentionmeter.value))
-    potVal = potentionmeter.value
+    potentiometer_value = potentionmeter.value
 
     # shows value of the potentiometer
-    print(potVal)
+    print(potentiometer_value)
 
-    # changes range from 0-65535 to 0-180
-    angle = potVal * (180/65535)
+    # changes potentiometer range from 0-65535 to 0-180
+    angle = potentiometer_value * (180/65535)
+
+    # set servo angle value to match potentiometer value
     my_servo.angle = angle
     time.sleep(1)
